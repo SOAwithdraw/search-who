13e6054f5532282cd00928b44040b70134d229c3
carboncoo <chenchi15@mails.tsinghua.edu.cn>
2017-05-22 19:56:58 +0800

    gaoqi

--- sw/search/code/news_search.py
+++ sw/search/code/news_search.py
@@ -6,13 +6,16 @@ import yaml
 import json
 import codecs
 import sys
+import jieba
+import jieba.posseg as pseg
 
 import baidunews
 import cluster
+import photo
 
 reload(sys)
 sys.setdefaultencoding("utf-8")
-jieba.enable_parallel()
+# jieba.enable_parallel()
 
 
 def nouns_extract(text_list, banned_list, max_length=20):
@@ -41,7 +44,17 @@ def nouns_extract(text_list, banned_list, max_length=20):
 
 
 def cluster_pages(all_info, th, tp1, tp2, banned_list):
-
+    '''
+        传入待聚类的页面信息all_info和一些参数，返回聚类的页面编号
+        Args:
+            all_info: 页面信息，形式为 [[page1],[page2],...], page = {'text':,'img':,'url':,'id',...}
+            th: 聚类阈值，采用层次聚类
+            tp1,tp2: 聚类方法，详见cluster.py
+            banned_list: 提取特征时禁止的词，一般为搜索的关键字
+        Returns:
+            clustered_page: 一个list，形式为[[class1_p1,class1_p2,...],[class2_p1,class2_p2,...],...]
+            finword: 每一类的关键字
+    '''
     info_num = len(all_info)
 
     url_list = []
@@ -56,19 +69,33 @@ def cluster_pages(all_info, th, tp1, tp2, banned_list):
 
     clustered_page = [[url_list[i] for i in c] for c in fin]
 
-    return clustered_page, featured_text, fin, finword
+    return clustered_page, finword
+
+
+def cluster_img(all_info):
+    photos = [x['img'] for x in all_info]
+    fin, mainphoto = photo.Cluster(photos)
+    return fin, mainphoto
 
 
 def search(name, describe=[], cache_dir="data"):
-
+    '''
+        主要搜索函数，传入名称和其他搜索信息，返回搜索结果
+        Args:
+            name: 搜索人名
+            descrive: 辅助信息，如机构名，地名等等
+            cache_dir: 搜索结果保存地址
+        Returns:
+            search_result: 搜索结果，形式为[(类别关键字，[[url1,title1],[url2,titl2],...]), (类别2), (类别3)...]
+    '''
     if not os.path.exists(cache_dir):
         os.mkdir(cache_dir)
 
     print("Search {0} with description {1}".format(name, describe))
 
     search_word = name if not describe else name + ' ' + ' '.join(describe)
-    search_filename = os.path.join(cache_dir, name + ".yaml")
-    dirname = os.path.join(cache_dir, name)
+    search_filename = os.path.join(cache_dir, search_word + ".yaml")
+    dirname = os.path.join(cache_dir, search_word)
 
     if os.path.exists(search_filename):
         print("Load from cache...")
@@ -76,7 +103,7 @@ def search(name, describe=[], cache_dir="data"):
             baidu_result = yaml.load(f)
     else:
         print("Start searching news...")
-        baidu_result = baidunews.get(search_word)
+        baidu_result = baidunews.get(search_word, newscnt=50)
         output = codecs.open(search_filename, "w", "utf-8")
         yaml.dump(baidu_result, default_flow_style=False, stream=output, indent=4, encoding='utf-8', allow_unicode=True, width=1000)
 
@@ -86,28 +113,34 @@ def search(name, describe=[], cache_dir="data"):
     if not os.path.exists(dirname):
         os.mkdir(dirname)
 
+    print(json.dumps(baidu_result, ensure_ascii=False))
+    print('Cluster by images.')
+    fin, mainphoto = cluster_img(baidu_result)
+    print(mainphoto)
+
     th, tp1, tp2 = 0.15, 0, 1
-    pages, featured_text, fin, finword = cluster_pages(baidu_result, th, tp1, tp2, banned_list)
+    print('Cluster by texts.')
+    pages, finword = cluster_pages(baidu_result, th, tp1, tp2, banned_list)
 
     search_result = []
     for i in range(len(pages)):
         class_info = []
         for page in pages[i]:
-            class_info.append(['URL', 'TITLE'])
+            class_info.append([baidu_result[page]['url'], baidu_result[page]['title']])
         search_result.append([finword[i], class_info])
 
     # for Debug
     print(pages)
     print(json.dumps(finword, ensure_ascii=False))
 
-    for i in range(len(pages)):
-        with codecs.open(dirname + '/' + str(i) + '.txt', 'w', 'utf-8') as fout:
-            for j in range(len(pages[i])):
-                fout.write('[' + str(pages[i][j]) + ']\n')
-                fout.write('\n'.join(baidu_result[pages[i][j]]['text']))
-                fout.write(json.dumps(featured_text[fin[i][j]], ensure_ascii=False))
-                fout.write('\n')
-                fout.write('\n===========\n')
+    # for i in range(len(pages)):
+    #     with codecs.open(dirname + '/' + str(i) + '.txt', 'w', 'utf-8') as fout:
+    #         for j in range(len(pages[i])):
+    #             fout.write('[' + str(pages[i][j]) + ']\n')
+    #             fout.write('\n'.join(baidu_result[pages[i][j]]['text']))
+    #             fout.write(json.dumps(featured_text[fin[i][j]], ensure_ascii=False))
+    #             fout.write('\n')
+    #             fout.write('\n===========\n')
 
     return search_result
 
--- sw/search/views.py
+++ sw/search/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render
 from django.urls import reverse
 from django.http import HttpResponse, HttpResponseRedirect
 
+import os
 import sys
 sys.path.append('search/code')
 import news_search
@@ -20,5 +21,11 @@ def index(request):
 
 def search_person(request):
     content = request.GET['person']
-
+    print(content.encode('utf-8'))
+    contents = content.split()
+    name = contents[0]
+    if len(contents) > 1:
+        news_search.search(name, ''.join(contents[1:]))
+    else:
+        news_search.search(name)
     return HttpResponse(content)
