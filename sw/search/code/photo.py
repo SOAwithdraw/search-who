70f8f4c78d8488c502347c45f3a581bf5fd7db78
general502570 <465383637@qq.com>
2017-05-22 21:08:36 +0800

    move photo.py

--- sw/search/code/photo.py
+++ sw/search/code/photo.py
@@ -1,93 +1,90 @@
 import urllib2
-import httplib
-import urllib
+import httplib, urllib
 import json
 
-
 def Group(photos):
-    headers = {
-        # Request headers
-        'Content-Type': 'application/json',
-        'Ocp-Apim-Subscription-Key': '0bb0d50d31034b4f8397ffacfb6e9185',
-    }
+        headers = {
+            # Request headers
+            'Content-Type': 'application/json',
+            'Ocp-Apim-Subscription-Key': 'a0b8976eaa1d49a8980e162b7d7741c1',
+            #'Ocp-Apim-Subscription-Key': '0bb0d50d31034b4f8397ffacfb6e9185',
+        }
 
-    params = urllib.urlencode({})
+        params = urllib.urlencode({})
 
-    body = json.dumps({"faceIds": photos})
-    print(body)
+        body = json.dumps({"faceIds": photos})
+        print(body)
 
-    try:
-        conn = httplib.HTTPSConnection('westus.api.cognitive.microsoft.com')
-        print('connect')
-        conn.request("POST", "/face/v1.0/group?%s" % params, body, headers)
-        print('requst')
-        response = conn.getresponse()
-        print('response')
-        data = response.read()
-        print('read')
-        print(data)
-        data = eval(data)
-        conn.close()
-    except Exception as e:
-        print("error")
-        return []
+        try:
+            conn = httplib.HTTPSConnection('westus.api.cognitive.microsoft.com')
+            print('connect')
+            conn.request("POST", "/face/v1.0/group?%s" % params, body, headers)
+            print('requst')
+            response = conn.getresponse()
+            print('response')
+            data = response.read()
+            print('read')
+            print(data)
+            data = eval(data)
+            conn.close()
+        except Exception as e:
+            print("error")
+            return []
 
-    return data
-
+        return data
 
 def Checkphoto(img):
-    headers = {
-        # Request headers
-        'Content-Type': 'application/json',
-        'Ocp-Apim-Subscription-Key': '0bb0d50d31034b4f8397ffacfb6e9185',
-    }
+        headers = {
+            # Request headers
+            'Content-Type': 'application/json',
+            'Ocp-Apim-Subscription-Key': 'a0b8976eaa1d49a8980e162b7d7741c1',
+            #'Ocp-Apim-Subscription-Key': '0bb0d50d31034b4f8397ffacfb6e9185',
+        }
 
-    params = urllib.urlencode({
-        # Request parameters
-        'returnFaceId': 'true',
-        'returnFaceLandmarks': 'false',
-        'returnFaceAttributes': 'age,gender',
-    })
+        params = urllib.urlencode({
+            # Request parameters
+            'returnFaceId': 'true',
+            'returnFaceLandmarks': 'false',
+            'returnFaceAttributes': 'age,gender',
+        })
 
-    body = json.dumps({"url": img})
-    print(body)
+        body = json.dumps({"url": img})
+        print(body)
 
-    try:
-        conn = httplib.HTTPSConnection('westus.api.cognitive.microsoft.com')
-        print('connect')
-        conn.request("POST", "/face/v1.0/detect?%s" % params, body, headers)
-        print('requst')
-        response = conn.getresponse()
-        data = response.read()
-        data = eval(data)
-        print("data:" + data)
-        conn.close()
-    except Exception as e:
-        print("Errer")
+        try:
+            conn = httplib.HTTPSConnection('westus.api.cognitive.microsoft.com')
+            print('connect')
+            conn.request("POST", "/face/v1.0/detect?%s" % params, body, headers)
+            print('requst')
+            response = conn.getresponse()
+            data = response.read()
+            data = eval(data)
+            conn.close()
+        except Exception as e:
+            print("Errer")
 
-    context = []
+        context = []
 
-    try:
-        tryid = data[0]['faceId']
-    except:
+        try:
+            tryid = data[0]['faceId']
+        except:
+            return context
+
+        for i in range(len(data)):
+            context.append(data[i]["faceAttributes"])
+            context[i]['id'] = data[i]['faceId']
+            context[i]['siz'] = data[i]['faceRectangle']['width'] * data[i]['faceRectangle']['height']
+            context[i]['url'] = img
+
+        print "context:", context
         return context
-
-    for i in range(len(data)):
-        context.append(data[i]["faceAttributes"])
-        context[i]['id'] = data[i]['faceId']
-        context[i]['siz'] = data[i]['faceRectangle']['width'] * data[i]['faceRectangle']['height']
-        context[i]['url'] = img
-
-    print "context:", context
-    return context
-
 
 def Cluster(photos):
     """Return 2 lists, one is [[14, 45, 47], [13, 24]], the other is ['http://isadb.jpg', 'htpp://bibjkbskd.jpg']"""
-    # Checkphoto("http://upload.ahwang.cn/2015/1216/1450232258493.jpg")
-
+    #Checkphoto("http://upload.ahwang.cn/2015/1216/1450232258493.jpg")
+    
     # id means api id, ind means returning ind
-    print photos
+    #print photos
     fo = open("ids.txt", "w")
     tot = len(photos)
     ids = []
@@ -112,7 +109,7 @@ def Cluster(photos):
             mapf[curid] = i
 
     bago_group = Group(bagi_group)
-    print 'bago_group', bago_group
+    print 'bago_group',  bago_group
 
     fin = []
     mainphoto = []
@@ -140,7 +137,6 @@ def Cluster(photos):
 
     return fin, mainphoto
 
-
 def Try():
 
     fi = open("ids.txt", "r")
@@ -150,5 +146,5 @@ def Try():
     fi.close()
     bago = Group(bagi)
 
-if __name__ == '__main__':
-    Try()
+Try()
+
